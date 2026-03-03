387f3a4b29141dd029df11f664561a0cf693a5d4
Josenil Soares <josenil.junior90@gmail.com>
2026-03-03 12:02:41 -0300

    Atualizando o Problem

--- src/components/home/problem/Problem.tsx
+++ src/components/home/problem/Problem.tsx
@@ -1,7 +1,7 @@
 function Problem() {
   return (
     <section className="bg-gray-100 py-16">
-      <div className="container mx-auto px-6">
+      <div className="container mx-auto px-6 ">
         {/* Título */}
         <div className="text-center mb-12">
           <h2 className="text-3xl font-bold text-gray-800">
@@ -14,23 +14,23 @@ function Problem() {
         </div>
 
         {/* Imagens */}
-        <div className="mt-10 grid grid-cols-1 md:grid-cols-4 gap-1">
+        <div className="mt-10 grid grid-cols-1 md:grid-cols-4 gap-5 max-w-5xl mx-auto">
           <img
             src="/lp-comp1.png"
             alt="Imagem 1"
-            className="w-full h-[340px] object-contain rounded-2xl"
+            className="w-full h-85 object-cover rounded-2xl md:col-span-2 shadow-lg"
           />
 
           <img
             src="/lp-comp2.png"
             alt="Imagem 2"
-            className="w-full h-[340px] object-contain rounded-2xl"
+            className="w-full h-85 object-cover rounded-2xl shadow-lg"
           />
 
           <img
             src="/lp-comp3.png"
             alt="Imagem 3"
-            className="w-full h-[340px] object-contain rounded-2xl"
+            className="w-full h-85 object-cover rounded-2xl shadow-lg"
           />
         </div>
       </div>
