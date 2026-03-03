d1af60259aba37384b6ef7bd2ddf1f1c70cfb097
Cesar Henrique <dev.cesarhenrique@gmail.com>
2026-03-03 12:11:13 -0300

    ajuste no hero

--- src/components/home/hero/Hero.tsx
+++ src/components/home/hero/Hero.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Hero() {
   return (
     <section id="hero">
-      <div className="w-full lg:min-h-screen h-200 bg-[url('/lp-1.png')] lg:bg-cover bg-center text-left">
+      <div className="wrelative w-full bg-[url('/lp-1.png')] bg-cover bg-center">
         <div className="p-16">
           <div>
             <h2 className="md:text-left lg:text-6xl md:text-4xl text-3xl text-slate-50 font-semibold text-center lg:text-left text-shadow-sm">
@@ -17,7 +17,7 @@ function Hero() {
           <div className="text-center lg:text-left md:text-left text-slate-50 lg:mt-160 md:mt-110 mt-90">
             <p className="lg:text-xl md:text-lg text-md font-bold text-shadow-sm mb-3">
               Na Vittare, você escolhe o nível de proteção ideal e personaliza
-              sua cobertura em poucos minutos. Sem burocracia. Sem surpresas
+              sua cobertura em poucos minutos.<br></br> Sem burocracia, Sem surpresas.
             </p>
             <div className="flex justify-center lg:justify-start md:justify-start items-center gap-4 flex-wrap">
               <button className="shadow hover:opacity-80 cursor-pointer rounded-md h-10 w-55 text-sm font-bold text-white bg-red-600">
